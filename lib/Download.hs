b92396a123d49b82cbadf9c8707475ac75254b05
Tom Snee <351700+tsnee@users.noreply.github.com>
2025-06-14 15:42:02 -0400

    Implement decodeClubReport and tests (#11)

--- lib/Download.hs
+++ lib/Download.hs
@@ -9,7 +9,14 @@ Module      : Download
 Description : Functions for downloading club performance reports.
 Maintainer  : tomsnee@gmail.com
 -}
-module Download (CsvOctetStream (..), download, downloadClubPerformanceStarting, parseFooter) where
+module Download
+  ( CsvOctetStream (..)
+  , download
+  , downloadClubPerformanceStarting
+  , parseFooter
+  , decodeClubReport
+  )
+where
 
 import Control.Monad.Reader (ask)
 import Data.ByteString.Lazy.Char8 qualified as BL8
@@ -157,17 +164,24 @@ download env spec@ClubPerformanceReportDescriptor{format} = do
 
 newtype CsvOctetStream = CsvOctetStream OctetStream
   deriving Accept via OctetStream
+
+-- | Decode a CSV club performance report.
+decodeClubReport :: BL8.ByteString -> Either String ClubPerformanceReport
+decodeClubReport bytes = do
+  let rows = BL8.lines bytes
+  (rawCsv, footer) <-
+    maybeToRight
+      ("Could not break " <> show (BL8.length bytes) <> " bytes into lines.")
+      $ unsnoc rows
+  (_, parsedCsv) <- decodeByName $ BL8.unlines rawCsv
+  (monthReported, dayOfRecord) <- parseFooter $ BL8.unpack footer
+  let YearMonth yearOfRecord monthOfRecord = dayPeriod dayOfRecord
+      yearReported = if monthReported <= monthOfRecord then yearOfRecord else pred yearOfRecord
+      records = toList parsedCsv
+  pure ClubPerformanceReport{dayOfRecord, month = YearMonth yearReported monthReported, records}
+
 instance MimeUnrender CsvOctetStream ClubPerformanceReport where
-  mimeUnrender _ bytes = do
-    let rows = BL8.lines bytes
-    (rawCsv, footer) <-
-      maybeToRight ("Could not break " <> show (BL8.length bytes) <> " bytes into lines.") $ unsnoc rows
-    (_, parsedCsv) <- decodeByName $ BL8.unlines rawCsv
-    (monthReported, dayOfRecord) <- parseFooter $ BL8.unpack footer
-    let YearMonth yearOfRecord monthOfRecord = dayPeriod dayOfRecord
-        yearReported = if monthReported <= monthOfRecord then yearOfRecord else pred yearOfRecord
-        records = toList parsedCsv
-    pure ClubPerformanceReport{dayOfRecord, month = YearMonth yearReported monthReported, records}
+  mimeUnrender _ = decodeClubReport
 
 -- Example footer: "Month of Apr, As of 05/01/2025"
 -- String type used by Cassava.
