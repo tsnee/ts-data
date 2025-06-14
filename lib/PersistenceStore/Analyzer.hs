4a46da4bb5efe3bb39066c3b3c3da336f14c338b
Tom Snee <351700+tsnee@users.noreply.github.com>
2025-06-14 04:48:16 -0400

    Update Analyzer analyze signature

--- lib/PersistenceStore/Analyzer.hs
+++ lib/PersistenceStore/Analyzer.hs
@@ -1,10 +1,9 @@
 module PersistenceStore.Analyzer (Analyzer, analyze) where
 
-import Data.List (List)
 import Data.Time (Day)
 
 import PersistenceStore.Measurement (Measurement (..))
 import Types.ClubNumber (ClubNumber)
 
 class Analyzer a b where
-  analyze :: ClubNumber -> Day -> a -> List (Measurement b)
+  analyze :: ClubNumber -> Day -> a -> [Measurement b]
